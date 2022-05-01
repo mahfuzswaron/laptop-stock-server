f1920fa8a88724f8b0cf83dab8993ee0f23f7ae0
mahfuzswaron <mahfuzswaron@gmail.com>
2022-05-01 17:37:43 +0600

    initial set up done

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express')
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 4000;
 
+app.use(cors())
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@laptop-stock-cluster.lbnux.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri);
+const laptopsCollection = client.db('laptop-db').collection('laptops')
 
-client.connect(err => {
-  const collection = client.db("laptop-db").collection("laptops");
-  // perform actions on the collection object
-  console.log('mongo added')
-  client.close();
+const run = async() =>{
+          try{
 
-});
+              await client.connect();
 
+              //get all documents
+              app.get('/laptops', async(req, res)=>{
+                  const cursor =  laptopsCollection.find({});
+                  const laptops = await cursor.toArray();
+                  res.send(laptops);
+              })
+          }
+          finally{
+
+          }
+}
+
+run().catch(e => console.log(e))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
