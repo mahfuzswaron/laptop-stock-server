c5b707b508f2583c5de0e0043dc16caebeed3de2
mahfuzswaron <mahfuzswaron@gmail.com>
2022-05-01 16:35:33 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@laptop-stock-cluster.lbnux.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+  const collection = client.db("laptop-db").collection("laptops");
+  // perform actions on the collection object
+  console.log('mongo added')
+  client.close();
+
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
