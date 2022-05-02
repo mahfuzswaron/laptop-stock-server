866e5c9fbac81988fc2ce1bb1688b276d1049130
mahfuzswaron <mahfuzswaron@gmail.com>
2022-05-02 19:14:00 +0600

    connected with client side for PUT

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 4000;
 
-app.use(cors())
+app.use(cors());
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@laptop-stock-cluster.lbnux.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -17,11 +18,26 @@ const run = async() =>{
 
               await client.connect();
 
-              //get all documents
+              //get all inventories
               app.get('/laptops', async(req, res)=>{
                   const cursor =  laptopsCollection.find({});
                   const laptops = await cursor.toArray();
                   res.send(laptops);
+              })
+
+              // get inventory by id
+              app.get('/laptops/:id', async(req, res)=>{
+                const id = req.params.id;
+                const query = {_id: ObjectId(id)};
+                const laptop = await laptopsCollection.findOne(query);
+                console.log(laptop)
+                res.send(laptop)
+              })
+
+              // add new inventory
+              app.put('/laptops/addnew', async(req, res)=>{
+                const newInventory = req.body;
+                console.log(newInventory);
               })
           }
           finally{
